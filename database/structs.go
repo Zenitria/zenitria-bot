6a3b60a7d5e95b2aabddc95027eace2c02ab7274
MomboteQ <momboteq@gmail.com>
2023-10-14 16:18:19 +0200

    [v1.1.1][CHANGE] expires to expiresAt, string to time.Time, Cash to cash

--- database/structs.go
+++ database/structs.go
@@ -1,22 +1,24 @@
 package database
 
+import "time"
+
 type User struct {
 	ID          string  `bson:"_id"`
 	Level       int     `bson:"level"`
 	XP          int     `bson:"xp"`
 	NextLevelXP int     `bson:"nextLevelXP"`
 	Warnings    int     `bson:"warnings"`
-	Cash        float32 `bson:"Cash"`
+	Cash        float32 `bson:"cash"`
 }
 
 type Code struct {
-	Code    string   `bson:"code"`
-	Amount  int      `bson:"amount"`
-	Expires string   `bson:"expires"`
-	Uses    int      `bson:"uses"`
-	Used    int      `bson:"used"`
-	Users   []string `bson:"users"`
-	IPs     []string `bson:"ips"`
+	Code      string    `bson:"code"`
+	Amount    int       `bson:"amount"`
+	ExpiresAt time.Time `bson:"expiresAt"`
+	Uses      int       `bson:"uses"`
+	Used      int       `bson:"used"`
+	Users     []string  `bson:"users"`
+	IPs       []string  `bson:"ips"`
 }
 
 type Channel struct {
