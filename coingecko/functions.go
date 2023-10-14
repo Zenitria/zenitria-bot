095090cfdf1cb9df5093f95959315136068d3ca7
MomboteQ <momboteq@gmail.com>
2023-10-14 16:18:49 +0200

    [v1.1.1][FIX] request

--- coingecko/functions.go
+++ coingecko/functions.go
@@ -11,7 +11,11 @@ func getPrices() Coins {
 	url := "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,monero,nano&vs_currencies=usd&include_24hr_change=true"
 
 	req, _ := http.NewRequest("GET", url, nil)
-	res, _ := http.DefaultClient.Do(req)
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		return Coins{}
+	}
 
 	if res.Body != nil {
 		defer res.Body.Close()
