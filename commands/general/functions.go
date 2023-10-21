ddc76b88ea01c543d61a1b42d813ab6a034b3761
MomboteQ <momboteq@gmail.com>
2023-10-21 21:28:41 +0200

    [v1.2.1][ADD] slash subcommand mentions

--- commands/general/functions.go
+++ commands/general/functions.go
@@ -9,16 +9,26 @@ import (
 func getSlashCommandMentions(s *discordgo.Session) map[string]string {
 	cmds, _ := s.ApplicationCommands(s.State.User.ID, "")
 
-	ids := make(map[string]string)
+	mentions := make(map[string]string)
 
 	for _, cmd := range cmds {
-		ids[cmd.Name] = cmd.ID
+		mentions[cmd.Name] = fmt.Sprintf("</%s:%s>", cmd.Name, cmd.ID)
 	}
+
+	return mentions
+}
+
+func getSlashSubcommandMentions(s *discordgo.Session) map[string]string {
+	cmds, _ := s.ApplicationCommands(s.State.User.ID, "")
 
 	mentions := make(map[string]string)
 
-	for name, id := range ids {
-		mentions[name] = fmt.Sprintf("</%s:%s>", name, id)
+	for _, cmd := range cmds {
+		for _, option := range cmd.Options {
+			if option.Type == discordgo.ApplicationCommandOptionSubCommand {
+				mentions[fmt.Sprintf("%s %s", cmd.Name, option.Name)] = fmt.Sprintf("</%s %s:%s>", cmd.Name, option.Name, cmd.ID)
+			}
+		}
 	}
 
 	return mentions
