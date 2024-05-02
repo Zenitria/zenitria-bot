9c2721162ddc892b64e02b42b53cdda5bc00963b
MomboteQ <momboteq@gmail.com>
2024-05-02 21:50:26 +0200

    [v1.3.0][ADD] New variables

--- config/config.go
+++ config/config.go
@@ -10,8 +10,12 @@ var (
 	TOKEN               string
 	MONGODB_URI         string
 	GET_XNO_API_URL     string
+	GET_BAN_API_URL     string
+	ZENITRIA_API_URL    string
 	COMMANDS_CHANNEL_ID string
 	OWNER_ID            string
+	NANSWAP_SECRET      string
+	NANO_SEED           string
 )
 
 func init() {
@@ -20,6 +24,10 @@ func init() {
 	TOKEN = os.Getenv("TOKEN")
 	MONGODB_URI = os.Getenv("MONGODB_URI")
 	GET_XNO_API_URL = os.Getenv("GET_XNO_API_URL")
+	GET_BAN_API_URL = os.Getenv("GET_BAN_API_URL")
+	ZENITRIA_API_URL = os.Getenv("ZENITRIA_API_URL")
 	COMMANDS_CHANNEL_ID = os.Getenv("COMMANDS_CHANNEL_ID")
 	OWNER_ID = os.Getenv("OWNER_ID")
+	NANSWAP_SECRET = os.Getenv("NANSWAP_SECRET")
+	NANO_SEED = os.Getenv("NANO_SEED")
 }
