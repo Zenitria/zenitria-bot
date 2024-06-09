bd492f464826e4b8404a692a34808b5f9f28fd49
MomboteQ <momboteq@gmail.com>
2024-06-09 16:26:27 +0200

    [v1.3.0][FIX] Get BAN stats

--- commands/general/stats/get_ban.go
+++ commands/general/stats/get_ban.go
@@ -12,7 +12,7 @@ func HandleGetBAN(s *discordgo.Session, i *discordgo.InteractionCreate) {
 
 	embed := &discordgo.MessageEmbed{
 		Title:       "📊・Get BAN stats",
-		Description: fmt.Sprintf("📆・**Days**: %d\n👥・**Users**: %d\n💸・**Withdrawn**: %.2f\n📈・**Claims**: %d (%s)", platform.Days, platform.Users, platform.Withdrawn, platform.Claims, change),
+		Description: fmt.Sprintf("📆・**Days**: %d\n👥・**Users**: %d\n💸・**Withdrawn**: %.2f BAN\n📈・**Claims**: %d (%s)", platform.Days, platform.Users, platform.Withdrawn, platform.Claims, change),
 		Color:       0xB54DFF,
 		Thumbnail: &discordgo.MessageEmbedThumbnail{
 			URL: "https://get-ban.com/_next/image?url=%2Fimages%2Fget-ban-logo.png&w=256&q=100",
